c38d9153aacdb61ce78a3d4af028fada8fccce56
ZhangYuxiang <zz593141477@gmail.com>
2017-04-10 17:48:49 +0800

    LED flash control

--- ZynqDanmaku-HDL/ZynqDanmaku.srcs/sources_1/new/led_pwm_ctl.v
+++ ZynqDanmaku-HDL/ZynqDanmaku.srcs/sources_1/new/led_pwm_ctl.v
@@ -9,15 +9,16 @@ module led_pwm_ctl (
 
 parameter integer WEAK_LIGHT_PULSE = 16384*10/100;
 
-reg [15:0] counter;
-wire weak_on = (counter < WEAK_LIGHT_PULSE);
+reg [24:0] counter;
+wire weak_on = (counter[15:0] < WEAK_LIGHT_PULSE);
+wire flash = counter[24];
 wire [4:0] led_out;
 
 always @(posedge clk_fabric) begin : proc_counter
     counter <= counter + 1;
 end
 
-assign led_out = enable & (active | (weak_on ? 5'b11111 : 5'b0));
+assign led_out = enable & (active & {flash,1'b1,flash,1'b1,1'b1} | (weak_on ? 5'b11111 : 5'b0));
 
 assign led_o_n = ~led_out;
 
