a8d3f1309cca6a2a21bd9a1aff08b5631e39ff74
ZhangYuxiang <zz593141477@gmail.com>
2017-05-05 13:09:03 +0800

    project files update

--- ZynqDanmaku-HDL/ZynqDanmaku.srcs/sources_1/ip/clk_wiz_0/clk_wiz_0_stub.v
+++ ZynqDanmaku-HDL/ZynqDanmaku.srcs/sources_1/ip/clk_wiz_0/clk_wiz_0_stub.v
@@ -1,10 +1,10 @@
 // Copyright 1986-2016 Xilinx, Inc. All Rights Reserved.
 // --------------------------------------------------------------------------------
 // Tool Version: Vivado v.2016.4 (lin64) Build 1733598 Wed Dec 14 22:35:42 MST 2016
-// Date        : Mon Apr  3 22:43:11 2017
+// Date        : Mon Apr  3 22:43:10 2017
 // Host        : skyworks running 64-bit Ubuntu 16.04.1 LTS
-// Command     : write_verilog -force -mode synth_stub
-//               /home/skyworks/ZynqDanmaku/ZynqDanmaku-HDL/ZynqDanmaku.srcs/sources_1/ip/clk_wiz_0/clk_wiz_0_stub.v
+// Command     : write_verilog -force -mode synth_stub -rename_top clk_wiz_0 -prefix
+//               clk_wiz_0_ clk_wiz_0_stub.v
 // Design      : clk_wiz_0
 // Purpose     : Stub declaration of top-level module interface
 // Device      : xc7z010clg400-1
